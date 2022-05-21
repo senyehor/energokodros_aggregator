06fe4d98db5a083ee0b9a34d266e96e1826eae96
senyehor <e.sencha2002@gmail.com>
2022-05-21 14:08:30 +0300

    decided to leave logs to console

--- main.go
+++ main.go
@@ -4,16 +4,11 @@ import (
 	"aggregator/app"
 	"aggregator/utils"
 	log "github.com/sirupsen/logrus"
-	"os"
 )
 
 func main() {
 	config := utils.GetAppConfig()
-	if config.Debug() {
-		log.SetLevel(log.DebugLevel)
-	}
-	logFile, _ := os.Create("./log")
-	log.SetOutput(logFile)
+
 	application := app.CreateApp(config)
 	iterationsMade := application.Aggregate()
 	log.Infof("Exiting, made %v iterations", iterationsMade)
